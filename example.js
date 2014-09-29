c84bc12228b79e2a1d056478e09ab746cfb88852
Rayo <surui.cc@gmail.com>
2014-09-29 13:23:30 +0800

    Update example.js

--- example.js
+++ example.js
@@ -4,4 +4,4 @@ require('hot-require');
 
 // use example:
 // you can use the global function _require to require your js file.
-var yourJs = _require(__dirname, '[module or js name you want to hot reload]');
+var yourJs = _require(__dirname, '[js path you want to hot reload]');
